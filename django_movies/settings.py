9fa7733f328a2c946d526c61400e262e3ff05666
Vlad Anokhin <vlad.anokhin@cleverr.com>
2021-12-15 21:10:25 +0200

    added new data in admin

--- django_movies/settings.py
+++ django_movies/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -126,3 +127,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Image URL and directory
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = Path(BASE_DIR, 'media')
--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register([
+    Category,
+    Actor,
+    Genre,
+    Movie,
+    MovieShots,
+    RatingStar,
+    Rating,
+    Reviews
+])
