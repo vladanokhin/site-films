365d2950829023d3eb650488f57540e911a5e863
Vlad Anokhin <vlad.anokhin@cleverr.com>
2022-01-13 21:32:54 +0200

    add view and new templates

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import MoviesView
+
 
 urlpatterns = [
-
+    path('', MoviesView.as_view())
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,2 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.base import View
+from .models import Movie
+
+
+class MoviesView(View):
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, 'movies.html', {'movies': movies})
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('POST request!')    
